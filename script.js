74d91520e7feffad9d2ce67066345575573dd4a9
Felix Edström <felix.edstrom1@gmail.com>
2023-04-07 00:58:27 +0200

    media querys

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const sendBtn = document.querySelector('#sendBtn')
+
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.utils.toArray(".panel").forEach((panel, i) => {
@@ -10,13 +12,93 @@ gsap.utils.toArray(".panel").forEach((panel, i) => {
     });
 });
 
-// gsap.utils.toArray(".manel").forEach((panel, i) => {
-//     ScrollTrigger.create({
-//         trigger: panel,
-//         start: "top top",
-//         pin: true,
-//         scrub: true,
-//         pinSpacing: true
-//     });
-// });
+
+const sendMail = () => {
+
+    const sendErrorText = document.querySelector('.sendErrorText')
+    const fullName = document.querySelector('#fullName')
+    const email = document.querySelector('#email_id')
+    const message = document.querySelector('#message')
+
+    // if (fullName.value.trim() === '' || email_id.value.trim() === '' || message.value.trim() === '') {
+    //     fullName.style.border = '.5px solid firebrick'
+    //     return
+    //   }
+
+    if (fullName.value.trim() === '') {
+        fullName.style.border = '.5px solid firebrick'
+        sendErrorText.style.visibility = 'visible'
+        sendErrorText.innerText = 'Please enter a valid name'
+        return
+    }
+    if (email.value.trim() === '') {
+        email.style.border = '.5px solid firebrick'
+        sendErrorText.style.visibility = 'visible'
+        sendErrorText.innerText = 'Please enter an email'
+        return
+    }
+
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(!email.value.match(emailRegex)) {
+        email.style.border = '.5px solid firebrick'
+        sendErrorText.style.visibility = 'visible'
+        sendErrorText.innerText = 'Please enter a valid email'
+        return
+    }
+
+    if (message.value.trim() === '') {
+        message.style.border = '.5px solid firebrick'
+        sendErrorText.style.visibility = 'visible'
+        sendErrorText.innerText = 'Please enter a message'
+        return
+    }
+    
+
+    const params = {
+        name: fullName.value,
+        email: email_id.value,
+        message: message.value
+    }
+
+
+    emailjs.send("service_6e8oaug", "template_uubfzg5", params)
+    .then(res => {
+
+        fullName.value = ''
+        email_id.value = ''
+        message.value = ''
+
+        gsap.fromTo("#emailSent", 1.25, { opacity: 0, x: 100 }, { opacity: 1, x: 0, ease: "elastic.out(1, 0.3)" } )
+    })
+    .catch(err => console.log(err))
+}
+
+sendBtn.addEventListener('click', e => {
+    e.preventDefault()
+    
+    sendMail()
+})
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger')
+    const slider = document.querySelector('#slider')
+    const navLinks = document.querySelectorAll('#slider li')
+    
+    burger.addEventListener('click', () => {
+        slider.classList.toggle('nav-active')
+
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`
+            }
+        })
+    })
+
+}
+
+navSlide()
 
