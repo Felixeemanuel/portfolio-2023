7208933c82ff34faffa4f6f76b16a100cfcabb2e
Felix Edström <felix.edstrom1@gmail.com>
2023-05-25 16:58:46 +0200

    carousel

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ backToTopBtn.addEventListener('click', toTop)
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (prevScrollpos > currentScrollPos || currentScrollPos == 0) {
     document.querySelector(".hero_navigation").style.top = "0";
   } else {
     document.querySelector(".hero_navigation").style.top = "-8vh";
